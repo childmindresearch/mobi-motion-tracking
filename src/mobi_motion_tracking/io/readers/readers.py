46e06834ac75aefa640552e1962c54c3d5737fc3
cgmaiorano <celiagrace13@gmail.com>
2025-01-30 14:31:12 -0500

    ruff formatting all files

--- src/mobi_motion_tracking/io/readers/readers.py
+++ src/mobi_motion_tracking/io/readers/readers.py
@@ -6,33 +6,32 @@ import pandas as pd
 
 def data_cleaner(data: pd.DataFrame) -> np.ndarray:
     """Select applicable data from a dataframe.
-    
+
     Currently motion tracking data for Kinect and Zed are saved into xlsx files.
-    Searches through dataframe extracted from the read_sheet function for 
+    Searches through dataframe extracted from the read_sheet function for
     x_hip, then extracts all rows below and the neighboring 61 columns.
-    
+
     Args:
         data: dataframe from read_sheet.
-    
+
     Returns:
-        cleaned_data: np.array of [x,61] where x is the total number of rows 
-            representing all frames and columns representing the frames and 60 joint 
+        cleaned_data: np.array of [x,61] where x is the total number of rows
+            representing all frames and columns representing the frames and 60 joint
             coordinates.
-        
+
     Raises:
         ValueError: when x_Hip is not found in dataframe.
     """
-    value_to_search = 'x_Hip'
+    value_to_search = "x_Hip"
     result = data.where(data == value_to_search).stack().index
-    
+
     if result.empty:
         raise ValueError("x_Hip not found in DataFrame.")
-    
+
     x_idx = result[0][0]
     y_idx = result[0][1]
     cleaned_data = data.iloc[
-        x_idx+1:, data.columns.get_loc(y_idx)-1:data.columns.get_loc(y_idx)+60
-        ].to_numpy()
-    
-    return cleaned_data
+        x_idx + 1 :, data.columns.get_loc(y_idx) - 1 : data.columns.get_loc(y_idx) + 60
+    ].to_numpy()
 
+    return cleaned_data
--- tests/unit/test_readers.py
+++ tests/unit/test_readers.py
@@ -14,30 +14,36 @@ def create_dummy_dataframe(valid: bool = True) -> pd.DataFrame:
         valid: When valid is True (default), a valid dataframe containing
         'x_Hip' at [2,5] will be generated. When valid is False, an invalid dataframe
         not containing 'x_Hip' will be generated.
-    
+
     Returns:
-        pd.DataFrame: A 5x70 DataFrame with numeric values and either a known 'x_Hip' 
+        pd.DataFrame: A 5x70 DataFrame with numeric values and either a known 'x_Hip'
         placement or a missing 'x_Hip' for testing.
     """
     if valid:
-        data = pd.DataFrame([
-            list(range(1, 71)),
-            ['a', 'b', 'c', 'd', 'e', 'x_Hip', 'g'] + list(range(8, 71)),
-            list(range(71, 141))
-        ])
+        data = pd.DataFrame(
+            [
+                list(range(1, 71)),
+                ["a", "b", "c", "d", "e", "x_Hip", "g"] + list(range(8, 71)),
+                list(range(71, 141)),
+            ]
+        )
     else:
-        data = pd.DataFrame([
-            list(range(1, 71)),
-            ['a', 'b', 'c', 'd', 'e', 'f', 'g'] + list(range(8, 71)),
-            list(range(71, 141))
-        ])
+        data = pd.DataFrame(
+            [
+                list(range(1, 71)),
+                ["a", "b", "c", "d", "e", "f", "g"] + list(range(8, 71)),
+                list(range(71, 141)),
+            ]
+        )
     return data
+
 
 def test_data_cleaner_invalid_dataframe() -> None:
     """Test the data_cleaner function with an invalid dataframe."""
     data = create_dummy_dataframe(valid=False)
     with pytest.raises(ValueError, match="x_Hip not found in DataFrame"):
         readers.data_cleaner(data)
+
 
 def test_data_cleaner_correct_size() -> None:
     """Test that data_cleaner extracts the correct shape from valid dummy data."""
@@ -46,16 +52,19 @@ def test_data_cleaner_correct_size() -> None:
     expected_cols = 61
 
     cleaned_data = readers.data_cleaner(data)
-    
+
     assert isinstance(cleaned_data, np.ndarray), "Output should be a NumPy array"
-    assert cleaned_data.shape == (expected_rows, expected_cols), (
-        f"Expected shape ({expected_rows}, {expected_cols}), \
-            but got {cleaned_data.shape}")
+    assert cleaned_data.shape == (
+        expected_rows,
+        expected_cols,
+    ), f"Expected shape ({expected_rows}, {expected_cols}), \
+            but got {cleaned_data.shape}"
+
 
 def test_data_cleaner_correct_values() -> None:
     """Test that data_cleaner extracts the correct known values from dummy data."""
     data = create_dummy_dataframe()
-    expected_output = [np.array(range(75,136))]
+    expected_output = [np.array(range(75, 136))]
 
     cleaned_data = readers.data_cleaner(data)
 
