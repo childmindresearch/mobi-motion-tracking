88530fb08ae3dc99ad3cf96ec37f8e262fe76f8a
cgmaiorano <113949028+cgmaiorano@users.noreply.github.com>
2025-02-03 13:08:50 -0500

    Feature/issue 4/manual data cleaner (#17)
    * initial commit for data_cleaner
    
    * created init files and began unit tests
    
    * wrote 2 more unit tests for data_cleaner function.
    
    * edited poetry.lock and poetry.toml
    
    * corrected unit tests and data_cleaner
    
    * ruff formatting all files
    
    * ruff format src/mobi_motion_tracking/__init__.py
    
    * pr requested edits
    
    * ruff format test_readers
    
    * Update src/mobi_motion_tracking/io/readers/readers.py
    
    Co-authored-by: Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
    
    * Resolves: Issue #4
    
    * resolve ruff check
    
    ---------
    
    Co-authored-by: Adam Santorelli <148909356+Asanto32@users.noreply.github.com>

---
+++ src/mobi_motion_tracking/io/readers/readers.py
@@ -0,0 +1,45 @@
+"""Functions to read motion tracking data from a file."""
+
+import numpy as np
+import pandas as pd
+
+
+def data_cleaner(data: pd.DataFrame) -> np.ndarray:
+    """Select applicable data from a dataframe.
+
+    Currently motion tracking data for Kinect and Zed are saved into xlsx files.
+    This function searches through dataframe extracted from the read_sheet function for
+    x_hip, then extracts all rows below and the neighboring 61 columns.
+
+    Args:
+        data: dataframe from read_sheet.
+
+    Returns:
+        cleaned_data: np.array of [x,61] where x is the total number of rows
+            representing all frames and columns representing the frame number and 60
+            joint coordinates.
+
+    Raises:
+        ValueError: when x_Hip is not found in dataframe.
+        IndexError: when column index is out of range.
+    """
+    result = data.where(data == "x_Hip").stack().index
+
+    if result.empty:
+        raise ValueError("x_Hip not found in DataFrame.")
+
+    row = result[0][0]
+    col_idx = result[0][1]
+
+    start_col = data.columns.get_loc(col_idx) - 1
+    end_col = data.columns.get_loc(col_idx) + 60
+
+    if start_col < 0 or end_col > data.shape[1]:
+        raise IndexError("Column index out of range.")
+
+    cleaned_data = data.iloc[
+        row + 1 :,
+        start_col:end_col,
+    ].to_numpy()
+
+    return cleaned_data
---
+++ tests/unit/test_readers.py
@@ -0,0 +1,72 @@
+"""test readers.py functions."""
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from mobi_motion_tracking.io.readers import readers
+
+
+def create_dummy_dataframe(valid: bool = True) -> pd.DataFrame:
+    """Create a known 5x70 dummy DataFrame for testing.
+
+    Args:
+        valid: When valid is True (default), a valid dataframe containing
+        'x_Hip' at [2,5] will be generated. When valid is False, an invalid dataframe
+        not containing 'x_Hip' will be generated.
+
+    Returns:
+        pd.DataFrame: A 5x70 DataFrame with numeric values and either a known 'x_Hip'
+        placement or a missing 'x_Hip' for testing.
+    """
+    if valid:
+        data = pd.DataFrame(
+            [
+                list(range(1, 71)),
+                ["a", "b", "c", "d", "e", "x_Hip", "g"] + list(range(8, 71)),
+                list(range(71, 141)),
+            ]
+        )
+    else:
+        data = pd.DataFrame(
+            [
+                list(range(1, 71)),
+                ["a", "b", "c", "d", "e", "f", "g"] + list(range(8, 71)),
+                list(range(71, 141)),
+            ]
+        )
+    return data
+
+
+def test_data_cleaner_invalid_dataframe() -> None:
+    """Test the data_cleaner function with an invalid dataframe."""
+    data = create_dummy_dataframe(valid=False)
+    with pytest.raises(ValueError, match="x_Hip not found in DataFrame."):
+        readers.data_cleaner(data)
+
+
+def test_data_cleaner_good() -> None:
+    """Test that data_cleaner extracts the correct data from valid dummy data."""
+    data = create_dummy_dataframe()
+    expected_rows = 1
+    expected_cols = 61
+    expected_output = [np.array(range(75, 136))]
+
+    cleaned_data = readers.data_cleaner(data)
+
+    assert isinstance(cleaned_data, np.ndarray), "Output should be a NumPy array."
+    assert cleaned_data.shape == (
+        expected_rows,
+        expected_cols,
+    ), f"Expected shape ({expected_rows}, {expected_cols}), \
+            but got {cleaned_data.shape}"
+    assert np.array_equal(cleaned_data, expected_output), "Extracted data does not \
+        match expected values."
+
+
+def test_data_cleaner_index_error() -> None:
+    """Test the data_cleaner function with an incorrect index length."""
+    data = create_dummy_dataframe()
+    data = data.iloc[:, :10]
+    with pytest.raises(IndexError, match="Column index out of range."):
+        readers.data_cleaner(data)
