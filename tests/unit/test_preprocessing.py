56944b56c9b5c747739917c392f670cf47ca9829
cgmaiorano <celiagrace13@gmail.com>
2025-02-12 10:58:20 -0500

    edits to test_preprocessing

--- tests/unit/test_preprocessing.py
+++ tests/unit/test_preprocessing.py
@@ -10,13 +10,13 @@ def test_normalize_joints_good() -> np.ndarray:
     """Test that the normalize joints function extracts correct known values."""
     data = np.array([
         list(range(1, 11)),
-        list(range(1, 11)),
-        list(range(1, 11)),
+        list(range(2, 12)),
+        list(range(3, 13)),
     ])
     expected_output = np.array([
         [1, 0, 0, 0, 3, 3, 3, 6, 6, 6],
-        [1, 0, 0, 0, 3, 3, 3, 6, 6, 6],
-        [1, 0, 0, 0, 3, 3, 3, 6, 6, 6],
+        [2, 0, 0, 0, 3, 3, 3, 6, 6, 6],
+        [3, 0, 0, 0, 3, 3, 3, 6, 6, 6],
     ])
 
     normalized_data = preprocessing.normalize_all_joints_to_hip(data)
