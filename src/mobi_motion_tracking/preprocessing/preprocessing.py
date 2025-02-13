5f892c42ad54dd780d375e3b5cd4fd69ddbcd898
cgmaiorano <celiagrace13@gmail.com>
2025-02-13 11:18:36 -0500

    wrote get_average_length function

--- src/mobi_motion_tracking/preprocessing/preprocessing.py
+++ src/mobi_motion_tracking/preprocessing/preprocessing.py
@@ -27,3 +27,32 @@ def center_joints_to_hip(data: np.ndarray) -> np.ndarray:
     normalized_data[:, 3::3] -= z_pelvis[:, np.newaxis]
 
     return normalized_data
+
+
+def get_average_length(
+    centered_data: np.ndarray, segment_list: np.ndarray
+) -> np.ndarray:
+    """Calculate the average lengths of all joint segments.
+
+    This function calculates the average length across all frames of all connecting
+    joint segments in the skeleton. The x, y,and z coordinates of specified starting
+    and ending joints based on segment_list are used to calculate the average distance
+    between the two joints for all frames.
+
+    Args:
+        centered_data: ndarray, centered data output from center_joints_to_hip.
+        segment_list: ndarray [X,2], containing starting and ending joint pairs for all
+        skeleton segments.
+
+    Returns:
+        ndarray [X,1], average distance between joints for all segments.
+    """
+    starting_joint = 3 * (segment_list[:, 0] - 1)
+    ending_joint = 3 * (segment_list[:, 1] - 1)
+
+    starting_points = centered_data[:, starting_joint[:, None] + np.arange(1, 4)]
+    ending_points = centered_data[:, ending_joint[:, None] + np.arange(1, 4)]
+
+    distances = np.linalg.norm(starting_points - ending_points, axis=2)
+
+    return distances.mean(axis=0, keepdims=True).T
