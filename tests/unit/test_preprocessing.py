746487bd6437ef8d595b2ee32fbd1c798d144549
cgmaiorano <celiagrace13@gmail.com>
2025-02-17 11:16:12 -0500

    ruff formatting

--- tests/unit/test_preprocessing.py
+++ tests/unit/test_preprocessing.py
@@ -8,46 +8,46 @@ from mobi_motion_tracking.preprocessing import preprocessing
 
 def test_center_joints_to_hip_good() -> None:
     """Test that the center joints function extracts correct known values."""
-    data = np.array([
-        list(range(1, 11)),
-        list(range(2, 12)),
-        list(range(3, 13)),
-    ])
-    expected_output = np.array([
-        [1, 0, 0, 0, 3, 3, 3, 6, 6, 6],
-        [2, 0, 0, 0, 3, 3, 3, 6, 6, 6],
-        [3, 0, 0, 0, 3, 3, 3, 6, 6, 6],
-    ])
+    data = np.array(
+        [
+            list(range(1, 11)),
+            list(range(2, 12)),
+            list(range(3, 13)),
+        ]
+    )
+    expected_output = np.array(
+        [
+            [1, 0, 0, 0, 3, 3, 3, 6, 6, 6],
+            [2, 0, 0, 0, 3, 3, 3, 6, 6, 6],
+            [3, 0, 0, 0, 3, 3, 3, 6, 6, 6],
+        ]
+    )
 
     normalized_data = preprocessing.center_joints_to_hip(data)
 
     assert isinstance(normalized_data, np.ndarray), "Output should be a NumPy array."
-    assert np.array_equal(normalized_data, expected_output), (
-        "Extracted data does not \
+    assert np.array_equal(normalized_data, expected_output), "Extracted data does not \
         match expected values."
-    )
 
 
 def test_get_average_length_good() -> None:
     """Test that the average length function calculates the expected value."""
     data = np.array([[1, 0, 0, 0, 1, 1, 1, 2, 2, 2], [2, 0, 0, 0, 1, 1, 1, 2, 2, 2]])
     segment_list = np.array([[0, 1], [0, 2], [1, 2]])
-    expected_output = np.array([
-        [1.7320508075688772],
-        [3.4641016151377544],
-        [1.7320508075688772],
-    ])
+    expected_output = np.array(
+        [
+            [1.7320508075688772],
+            [3.4641016151377544],
+            [1.7320508075688772],
+        ]
+    )
 
     average_length = preprocessing.get_average_length(data, segment_list)
 
-    assert np.array_equal(average_length, expected_output), (
-        "Calculated data does not \
+    assert np.array_equal(average_length, expected_output), "Calculated data does not \
         match expected values."
-    )
-    assert average_length.shape == (3, 1), (
-        f"Expected shape (3, 1), but got \
+    assert average_length.shape == (3, 1), f"Expected shape (3, 1), but got \
         {average_length.shape}"
-    )
     assert isinstance(average_length, np.ndarray), "Output should be a NumPy array."
 
 
