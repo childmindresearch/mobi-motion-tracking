df77750dadd697919bc2015124e180067b9aa333
cgmaiorano <113949028+cgmaiorano@users.noreply.github.com>
2025-02-21 13:48:04 -0500

    6 task write the calculate gold average edge lengths function (#24)
    * wrote get_average_length function
    
    * edits to get_average_length to ensure segment_list is zero indexed, wrote 3 unit tests
    
    * ruff formatting
    
    * review edits - incomplete
    
    * edit to assertion comment to be more specific
    
    * added pytest.mark.parametrize to test for negative and greater than index values
    
    * ruff format
    
    * resolving requested changes
    
    * created separate file for segment list and indexed according to array
    
    * rewrote JOINT_INDEX_LIST, rewrote get_average_length to use JOINT_INDEX_LIST
    
    * edits to docstring in get_average_length
    
    * correcting get_average_length and unit tests
    
    * edits to get_average_length docstring and unit tests
    
    * ruff formatting
    
    * edits to docstring
    
    * changed segments to be list of tuples of tuples
    
    * fixed indexing in get_average_length for tuples, changed unit test to use default segment array
    
    * renamed segments
    
    * edits to imports and docstring
    
    * added back unit test for dummy segment list
    
    * ruff format
    
    * last freymon and adam edits
    
    * Rename JOINT_INDEX_LIST.py to joint_index_list.py
    
    * ruff format

--- tests/unit/test_preprocessing.py
+++ tests/unit/test_preprocessing.py
@@ -1,6 +1,7 @@
 """test readers.py functions."""
 
 import numpy as np
+import pytest
 
 from mobi_motion_tracking.preprocessing import preprocessing
 
@@ -27,3 +28,54 @@ def test_center_joints_to_hip_good() -> None:
     assert isinstance(normalized_data, np.ndarray), "Output should be a NumPy array."
     assert np.array_equal(normalized_data, expected_output), "Extracted data does not \
         match expected values."
+
+
+def test_get_average_length_good_with_dummy_data() -> None:
+    """Test that the average length function calculates the expected value."""
+    data = np.array([[1, 0, 0, 0, 1, 1, 1, 2, 2, 2], [2, 0, 0, 0, 1, 1, 1, 2, 2, 2]])
+    segment_list = [
+        np.array([np.array([1, 4]), np.array([2, 5]), np.array([3, 6])]),
+        np.array([np.array([4, 7]), np.array([5, 8]), np.array([6, 9])]),
+    ]
+    expected_output = np.array([np.sqrt([3]), np.sqrt([3])])
+
+    average_length = preprocessing.get_average_length(data, segment_list)
+
+    assert np.array_equal(
+        average_length, expected_output
+    ), f"Calculated data {average_length} does not match expected values \
+            {expected_output}."
+    assert average_length.shape == (2, 1), f"Expected shape (2, 1), but got \
+        {average_length.shape}"
+    assert isinstance(average_length, np.ndarray), "Output should be a NumPy array."
+
+
+def test_get_average_length_good_with_default() -> None:
+    """Test that the average length function calculates the expected value."""
+    data = np.ones((2, 61))
+    expected_output = np.zeros((19, 1))
+
+    average_length = preprocessing.get_average_length(data)
+
+    assert np.array_equal(
+        average_length, expected_output
+    ), f"Calculated data {average_length} does not match expected values \
+            {expected_output}."
+    assert average_length.shape == (19, 1), f"Expected shape (19, 1), but got \
+        {average_length.shape}"
+    assert isinstance(average_length, np.ndarray), "Output should be a NumPy array."
+
+
+def test_get_average_length_out_of_range() -> None:
+    """Test the get_average_length function with a joint index out of range."""
+    data = np.array([[1, 0, 0, 0, 1, 1, 1, 2, 2, 2], [2, 0, 0, 0, 1, 1, 1, 2, 2, 2]])
+    segment_list = segment_list = [
+        np.array([np.array([1, 4]), np.array([2, 5]), np.array([3, 6])]),
+        np.array([np.array([4, 7]), np.array([5, 8]), np.array([6, 10])]),
+    ]
+    with pytest.raises(
+        IndexError,
+        match="Incorrect joint index list. Joint index in \
+                         segment_list is out of range for data.",
+    ):
+        preprocessing.get_average_length(data, segment_list)
