437b131167a105fea50ff549e4097a02772696e0
cgmaiorano <celiagrace13@gmail.com>
2025-03-03 13:44:48 -0500

    edits to docstring

--- src/mobi_motion_tracking/processing/models.py
+++ src/mobi_motion_tracking/processing/models.py
@@ -8,20 +8,25 @@ import numpy as np
 
 @dataclass
 class SimilarityMetrics:
-    """Stores similarity metrics between two time-series sequences."""
+    """Stores similarity metrics between two time-series sequences.
+
+    The method attribute contains the name of the similarity function used.
+    The metrics attribute is a string containing speciifc outputs for each
+    similarity function.
+    """
 
     method: str
     metrics: Dict[str, Any] = field(default_factory=dict)
 
     @classmethod
     def from_dtw(
-        cls, distance: float, path: list[tuple[int, int]]
+        cls, distance: float, warping_path: list[tuple[int, int]]
     ) -> "SimilarityMetrics":
         """Creates a SimilarityMetrics instance from DTW output.
 
         Args:
             distance: The cumulative distance between experimental and target sequences.
-            path: A list of tuples representing the warping path.
+            warping_path: A list of tuples representing the warping path.
 
         Returns:
             A SimilarityMetrics instance storing DTW-specific metrics.
@@ -30,7 +35,7 @@ class SimilarityMetrics:
             method="DTW",
             metrics={
                 "distance": distance,
-                "target_path": np.array([p[0] for p in path]),
-                "experimental_path": np.array([p[1] for p in path]),
+                "target_path": np.array([p[0] for p in warping_path]),
+                "experimental_path": np.array([p[1] for p in warping_path]),
             },
         )
