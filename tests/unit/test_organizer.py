c76dd1a1f5819498529f503997e5ae86ca046f61
cgmaiorano <celiagrace13@gmail.com>
2025-03-04 10:40:14 -0500

    rewrote unit tests

--- tests/unit/test_organizer.py
+++ tests/unit/test_organizer.py
@@ -1,26 +1,32 @@
 """Test organizer.py functions."""
 
-from pathlib import Path
+import pathlib
 
 import pytest
 
-from mobi_motion_tracking.core import organizer
+from mobi_motion_tracking.core import models
 
 
 def test_get_metadata_incorrect_filename() -> None:
     """Test get_metadata with an incorrect filename."""
     with pytest.raises(ValueError, match="The participant file is named incorrectly."):
-        organizer.get_metadata(Path("/dummy/path/100_01.xlsx"), 1)
+        models.Metadata.get_metadata(pathlib.Path("/dummy/path/100_01.xlsx"), 1)
 
 
 def test_get_metadata_good() -> None:
-    """Test get_metadata with an incorrect filename."""
+    """Test get_metadata works."""
     expected_ID = "100"
     expected_seq = "seq1"
 
-    participant_ID, sequence = organizer.get_metadata(Path("/dummy/path/100.xlsx"), 1)
+    metadata = models.Metadata.get_metadata(pathlib.Path("/dummy/path/100.xlsx"), 1)
 
-    assert isinstance(participant_ID, str), "participant_ID should be a string."
-    assert isinstance(sequence, str), "sequence should be a string."
-    assert expected_ID == participant_ID, "extracted ID does not match expected value."
-    assert expected_seq == sequence, "extracted sequence does not match expected value."
+    assert isinstance(
+        metadata.participant_ID, str
+    ), "participant_ID should be a string."
+    assert isinstance(metadata.sequence_sheetname, str), "sequence should be a string."
+    assert (
+        expected_ID == metadata.participant_ID
+    ), "extracted ID does not match expected value."
+    assert (
+        expected_seq == metadata.sequence_sheetname
+    ), "extracted sequence does not match expected value."
