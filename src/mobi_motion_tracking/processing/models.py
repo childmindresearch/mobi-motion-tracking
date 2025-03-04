94934f7120393f9e955b739bbaa42369f2e5e187
cgmaiorano <113949028+cgmaiorano@users.noreply.github.com>
2025-03-04 09:04:26 -0500

    8 task write dynamic time warping function (#26)
    * created processing submodule and __init__
    
    * created processing.py
    
    * wrote docstring for dtw function
    
    * wrote dtw function
    
    * created test_processing.py
    
    * wrote first unit test
    
    * poetry add fastdtw
    
    * poetry add scipy
    
    * changes to dtw package, dtw function, and unit test
    
    * more unit tests and edge case checks
    
    * edits to valuerror message
    
    * ruff format
    
    * edits for mypy error
    
    * created models.py and SimilarityMetrics class
    
    * edits to function to return dataclass
    
    * edits to models.py formatting, edits to dtw function, edits to unit tests
    
    * ruff format
    
    * edits to type hinting
    
    * fix type hinting
    
    * edits to similarity metrics class
    
    * edits to type hinting
    
    * mypy resolve and ruff format
    
    * Update pyproject.toml
    
    Adding support for newer python versions
    
    * renamed processing.py to similarity_functions.py
    
    * changed import level to models
    
    * removed asserts and added 1 assert
    
    * removed asserts
    
    * edits to docstring
    
    * rename to warping path
    
    * poetry lock
    
    * Update tests/unit/test_processing.py
    
    Co-authored-by: Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
    
    * edits to docstring
    
    ---------
    
    Co-authored-by: Adam Santorelli <148909356+Asanto32@users.noreply.github.com>

---
+++ src/mobi_motion_tracking/processing/models.py
@@ -0,0 +1,42 @@
+"""Dataclass storing all similarity metrics."""
+
+from dataclasses import dataclass, field
+from typing import Any, Dict
+
+import numpy as np
+
+
+@dataclass
+class SimilarityMetrics:
+    """Stores similarity metrics between two time-series sequences.
+
+    Attributes:
+        method: a string containing the name of the similarity function used.
+        metrics a list containing speciifc outputs for each
+            similarity function.
+    """
+
+    method: str
+    metrics: Dict[str, Any] = field(default_factory=dict)
+
+    @classmethod
+    def from_dtw(
+        cls, distance: float, warping_path: list[tuple[int, int]]
+    ) -> "SimilarityMetrics":
+        """Creates a SimilarityMetrics instance from DTW output.
+
+        Args:
+            distance: The cumulative distance between experimental and target sequences.
+            warping_path: A list of tuples representing the warping path.
+
+        Returns:
+            A SimilarityMetrics instance storing DTW-specific metrics.
+        """
+        return cls(
+            method="DTW",
+            metrics={
+                "distance": distance,
+                "target_path": np.array([p[0] for p in warping_path]),
+                "experimental_path": np.array([p[1] for p in warping_path]),
+            },
+        )
