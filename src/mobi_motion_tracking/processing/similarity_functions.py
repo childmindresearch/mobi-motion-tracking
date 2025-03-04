40ad771050043c88ec58bc0aba98566c20fa6607
cgmaiorano <113949028+cgmaiorano@users.noreply.github.com>
2025-03-04 09:04:26 -0500

    8 task write dynamic time warping function (#26)
    * created processing submodule and __init__
    
    * created processing.py
    
    * wrote docstring for dtw function
    
    * wrote dtw function
    
    * created test_processing.py
    
    * wrote first unit test
    
    * poetry add fastdtw
    
    * poetry add scipy
    
    * changes to dtw package, dtw function, and unit test
    
    * more unit tests and edge case checks
    
    * edits to valuerror message
    
    * ruff format
    
    * edits for mypy error
    
    * created models.py and SimilarityMetrics class
    
    * edits to function to return dataclass
    
    * edits to models.py formatting, edits to dtw function, edits to unit tests
    
    * ruff format
    
    * edits to type hinting
    
    * fix type hinting
    
    * edits to similarity metrics class
    
    * edits to type hinting
    
    * mypy resolve and ruff format
    
    * Update pyproject.toml
    
    Adding support for newer python versions
    
    * renamed processing.py to similarity_functions.py
    
    * changed import level to models
    
    * removed asserts and added 1 assert
    
    * removed asserts
    
    * edits to docstring
    
    * rename to warping path
    
    * poetry lock
    
    * Update tests/unit/test_processing.py
    
    Co-authored-by: Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
    
    * edits to docstring
    
    ---------
    
    Co-authored-by: Adam Santorelli <148909356+Asanto32@users.noreply.github.com>

---
+++ src/mobi_motion_tracking/processing/similarity_functions.py
@@ -0,0 +1,40 @@
+"""Functions for calculating similarity metrics on preprocessed data."""
+
+import numpy as np
+from dtaidistance import dtw
+
+from mobi_motion_tracking.processing import models
+
+
+def dynamic_time_warping(
+    preprocessed_target_data: np.ndarray, preprocessed_experimental_data: np.ndarray
+) -> models.SimilarityMetrics:
+    """Perform dynamic time warping.
+
+    This function calculates the cumulative distance and warping paths between the
+    target and experimental sequences. Both sequences have been centered to the
+    hip and the experimental sequence has been normalized to the target lengths.
+    Both input sequences have 61 columns. Column 0 contains frame number. Columns 1-3
+    contain the x, y, and z coordinates of the hip. Since the data has already been
+    centered to the hip, when calculating the distance and paths between the sequences,
+    we index the data starting at the 4th column until the end. This function returns
+    a dataclass which stores the DTW similarity metrics, distance and paths.
+
+
+    Args:
+        preprocessed_target_data: cleaned and centered target data.
+        preprocessed_experimental_data: cleaned, centered, and normalized
+            experimental data.
+
+    Returns:
+        SimilarityMetrics: a dataclass which stores the DTW similarity metrics.
+    """
+    distance, paths = dtw.warping_paths(
+        preprocessed_target_data[:, 4:].flatten(),
+        preprocessed_experimental_data[:, 4:].flatten(),
+    )
+    warping_path = dtw.best_path(paths)
+
+    return models.SimilarityMetrics.from_dtw(
+        distance=distance, warping_path=warping_path
+    )
