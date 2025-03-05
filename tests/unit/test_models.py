4590441133d49bb3ad04b03dd6f470b6be3abc2c
cgmaiorano <celiagrace13@gmail.com>
2025-03-05 13:43:04 -0500

    wrote new unit test for from_dtw in test_models and edited a previous unit test due to redundancy

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -2,6 +2,7 @@
 
 import pathlib
 
+import numpy as np
 import pytest
 
 from mobi_motion_tracking.core import models
@@ -29,4 +30,54 @@ def test_get_metadata_good() -> None:
     )
     assert expected_seq == metadata.sequence_sheetname, (
         "extracted sequence does not match expected value."
+    )
+
+
+def test_from_dtw_good() -> None:
+    """Test from_dtw works."""
+    distance = 1.0
+    warping_path = [[0, 1], [2, 3]]
+
+    expected_method = "DTW"
+    expected_distance = 1.0
+    expected_target_path = np.array([0, 2])
+    expected_experimental_path = np.array([1, 3])
+
+    similaritymetrics = models.SimilarityMetrics.from_dtw(
+        distance=distance, warping_path=warping_path
+    )
+    assert similaritymetrics.method == expected_method, (
+        f"Returned method {similaritymetrics.method} does not equal expected method \
+            {expected_method}."
+    )
+    assert similaritymetrics.metrics["distance"] == expected_distance, (
+        f"Calculated distance {similaritymetrics.metrics['distance']} does not \
+            match expected output {expected_distance}."
+    )
+    assert np.array_equal(
+        similaritymetrics.metrics["target_path"], expected_target_path
+    ), (
+        f"Calculated target path {similaritymetrics.metrics['target_path']} does \
+            not match expected output {expected_target_path}."
+    )
+    assert np.array_equal(
+        similaritymetrics.metrics["experimental_path"], expected_experimental_path
+    ), (
+        f"Calculated experimental path \
+            {similaritymetrics.metrics['experimental_path']} does not match expected \
+                output {expected_experimental_path}."
+    )
+    assert isinstance(similaritymetrics.method, str), (
+        "Returned method should be a string."
+    )
+    assert isinstance(similaritymetrics.metrics["distance"], float), (
+        "Ouput distance should be a float."
+    )
+    assert isinstance(similaritymetrics.metrics["target_path"], np.ndarray), (
+        "Output target path should be a NumPy \
+            array."
+    )
+    assert isinstance(similaritymetrics.metrics["experimental_path"], np.ndarray), (
+        "Output experimental path should \
+            be a NumPy array."
     )
--- tests/unit/test_similarity_functions.py
+++ tests/unit/test_similarity_functions.py
@@ -7,16 +7,12 @@ from mobi_motion_tracking.processing import similarity_functions
 
 def test_dtw_good() -> None:
     """Test that the dynamic time warping funciton extracts correct known values."""
-    preprocessed_target_data = np.array(
-        [
-            [0, 0, 0, 0, 0, 2, 4],
-        ]
-    )
-    preprocessed_experimental_data = np.array(
-        [
-            [0, 0, 0, 0, 1, 3, 5],
-        ]
-    )
+    preprocessed_target_data = np.array([
+        [0, 0, 0, 0, 0, 2, 4],
+    ])
+    preprocessed_experimental_data = np.array([
+        [0, 0, 0, 0, 1, 3, 5],
+    ])
     expected_target_path = np.array([0, 1, 2])
     expected_experimental_path = np.array([0, 1, 2])
     expected_distance = np.sqrt(3)
@@ -25,54 +21,43 @@ def test_dtw_good() -> None:
         preprocessed_target_data, preprocessed_experimental_data
     )
 
-    assert (
-        output.metrics["distance"] == expected_distance
-    ), f"Calculated distance {output.metrics['distance']} does not \
+    assert output.metrics["distance"] == expected_distance, (
+        f"Calculated distance {output.metrics['distance']} does not \
         match expected output {expected_distance}."
-    assert np.array_equal(
-        output.metrics["target_path"], expected_target_path
-    ), f"Calculated target path {output.metrics['target_path']} does not match \
+    )
+    assert np.array_equal(output.metrics["target_path"], expected_target_path), (
+        f"Calculated target path {output.metrics['target_path']} does not match \
             expected output {expected_target_path}."
+    )
     assert np.array_equal(
         output.metrics["experimental_path"], expected_experimental_path
-    ), f"Calculated experimental path {output.metrics['experimental_path']} \
+    ), (
+        f"Calculated experimental path {output.metrics['experimental_path']} \
             does not match expected output {expected_experimental_path}."
-    assert isinstance(
-        output.metrics["distance"], float
-    ), "Ouput distance should be a float."
-    assert isinstance(
-        output.metrics["target_path"], np.ndarray
-    ), "Output target path should be a NumPy \
-        array."
-    assert isinstance(
-        output.metrics["experimental_path"], np.ndarray
-    ), "Output experimental path should \
-        be a NumPy array."
+    )
 
 
 def test_dtw_identical_sequences() -> None:
     """Test the dtw function when target and experimental sequences are equal."""
-    preprocessed_target_data = np.array(
-        [
-            [0, 0, 0, 0, 1, 2, 3],
-        ]
-    )
-    preprocessed_experimental_data = np.array(
-        [
-            [0, 0, 0, 0, 1, 2, 3],
-        ]
-    )
+    preprocessed_target_data = np.array([
+        [0, 0, 0, 0, 1, 2, 3],
+    ])
+    preprocessed_experimental_data = np.array([
+        [0, 0, 0, 0, 1, 2, 3],
+    ])
     expected_distance = 0.0
 
     output = similarity_functions.dynamic_time_warping(
         preprocessed_target_data, preprocessed_experimental_data
     )
 
-    assert (
-        output.metrics["distance"] == expected_distance
-    ), f"Calculated distance {output.metrics['distance']} does not \
+    assert output.metrics["distance"] == expected_distance, (
+        f"Calculated distance {output.metrics['distance']} does not \
         match expected output {expected_distance}."
+    )
     assert np.array_equal(
         output.metrics["experimental_path"], output.metrics["target_path"]
-    ), f"Calculated experimental path {output.metrics['experimental_path']} and \
+    ), (
+        f"Calculated experimental path {output.metrics['experimental_path']} and \
             calculated target path {output.metrics['target_path']} are not equal."
+    )
