9e401a54bb3724879fdfdde0be83797cf657fd2d
cgmaiorano <celiagrace13@gmail.com>
2025-03-13 14:07:21 -0400

    rewrote writer function to append to a ndjson instead of regular json. avoids reloading the entire file to append every time and better for larger datasets

--- src/mobi_motion_tracking/io/writers/writers.py
+++ src/mobi_motion_tracking/io/writers/writers.py
@@ -6,20 +6,21 @@ import pathlib
 from mobi_motion_tracking.core import models
 
 
-def save_results_to_json(
-    metadata: models.Metadata,
+def save_results_to_ndjson(
+    gold_metadata: models.Metadata,
+    subject_metadata: models.Metadata,
     similarity_metrics: models.SimilarityMetrics,
     output_path: pathlib.Path,
 ) -> None:
-    """Writes output to json file."""
-    results = [
-        {
-            "participant_ID": metadata.participant_ID,
-            "sheetname": metadata.sequence_sheetname,
-            "method": similarity_metrics.method,
-            "metric": similarity_metrics.metrics,
-        }
-    ]
+    """Appends results to a newline-delimited JSON file (NDJSON)."""
+    new_entry = {
+        "gold_filename": gold_metadata.participant_ID,
+        "participant_ID": subject_metadata.participant_ID,
+        "sheetname": subject_metadata.sequence_sheetname,
+        "method": similarity_metrics.method,
+        "metric": similarity_metrics.metrics,
+    }
 
-    with open(output_path, "w") as f:
-        json.dump(results, f, indent=4)
+    with open(output_path, "a") as f:
+        json.dump(new_entry, f)
+        f.write("\n")
