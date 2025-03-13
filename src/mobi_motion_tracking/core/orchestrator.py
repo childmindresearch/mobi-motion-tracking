3dd885f7b00ceaf860f3686226e908edc5d50363
cgmaiorano <celiagrace13@gmail.com>
2025-03-13 13:49:23 -0400

    moved writer function to writers

--- src/mobi_motion_tracking/core/orchestrator.py
+++ src/mobi_motion_tracking/core/orchestrator.py
@@ -25,25 +25,6 @@ def run_algorithm(
     return simililarity_metric
 
 
-def save_results_to_json(
-    metadata: models.Metadata,
-    similarity_metrics: models.SimilarityMetrics,
-    output_path: pathlib.Path,
-) -> None:
-    """Writes output to json file."""
-    results = [
-        {
-            "participant_ID": metadata.participant_ID,
-            "sheetname": metadata.sequence_sheetname,
-            "method": similarity_metrics.method,
-            "metric": similarity_metrics.metrics,
-        }
-    ]
-
-    with open(output_path, "w") as f:
-        json.dump(results, f, indent=4)
-
-
 def run(
     gold_path: pathlib.Path,
     experimental_path: pathlib.Path,
--- src/mobi_motion_tracking/io/writers/writers.py
+++ src/mobi_motion_tracking/io/writers/writers.py
@@ -1,1 +1,25 @@
 """Functions to write calculated outputs to a file."""
+
+import json
+import pathlib
+
+from mobi_motion_tracking.core import models
+
+
+def save_results_to_json(
+    metadata: models.Metadata,
+    similarity_metrics: models.SimilarityMetrics,
+    output_path: pathlib.Path,
+) -> None:
+    """Writes output to json file."""
+    results = [
+        {
+            "participant_ID": metadata.participant_ID,
+            "sheetname": metadata.sequence_sheetname,
+            "method": similarity_metrics.method,
+            "metric": similarity_metrics.metrics,
+        }
+    ]
+
+    with open(output_path, "w") as f:
+        json.dump(results, f, indent=4)
