ad9b6d3341939df667263ba2a45ee78c3f0b6112
cgmaiorano <celiagrace13@gmail.com>
2025-03-18 11:58:42 -0400

    removed pytest_mock

--- tests/unit/test_orchestrator.py
+++ tests/unit/test_orchestrator.py
@@ -5,13 +5,12 @@ from typing import Tuple
 
 import numpy as np
 import pytest
-import pytest_mock
 
 from mobi_motion_tracking.core import orchestrator
 
 
 @pytest.fixture
-def mock_paths(tmp_path: pathlib.Path) -> Tuple[pathlib.Path, pathlib.path]:
+def mock_paths(tmp_path: pathlib.Path) -> Tuple[pathlib.Path, pathlib.Path]:
     """Creates temporary mock paths for gold and experimental files."""
     gold_path = tmp_path / "gold.xlsx"
     experimental_path = tmp_path / "experimental"
@@ -21,29 +20,8 @@ def mock_paths(tmp_path: pathlib.Path) -> Tuple[pathlib.Path, pathlib.path]:
     return gold_path, experimental_path
 
 
-@pytest.fixture
-def mock_dependencies(mocker: pytest_mock.MockerFixture) -> None:
-    """Mocks external dependencies of the run function."""
-    mocker.patch(
-        "your_module.models.Metadata.get_metadata",
-        return_value={"sequence_sheetname": "Sheet1"},
-    )
-    mocker.patch("your_module.readers.read_sheet", return_value="mock_data")
-    mocker.patch(
-        "your_module.preprocessing.center_joints_to_hip", return_value="centered_data"
-    )
-    mocker.patch(
-        "your_module.preprocessing.get_average_length", return_value="avg_lengths"
-    )
-    mocker.patch(
-        "your_module.preprocessing.normalize_segments", return_value="normalized_data"
-    )
-    mocker.patch("your_module.run_algorithm", return_value="mock_similarity_metric")
-    mocker.patch("your_module.writers.save_results_to_ndjson")
-
-
 def test_run_with_valid_directory(
-    mock_paths: Tuple[pathlib.Path, pathlib.Path], mock_dependencies: None
+    mock_paths: Tuple[pathlib.Path, pathlib.Path],
 ) -> None:
     """Test run funciton with a valid directory."""
     gold_path, experimental_path = mock_paths
@@ -53,28 +31,14 @@ def test_run_with_valid_directory(
 
     orchestrator.run(gold_path, experimental_dir, sequence, algorithm)
 
-    mock_dependencies["models.Metadata.get_metadata"].assert_called()
-    mock_dependencies["readers.read_sheet"].assert_called()
-    mock_dependencies["preprocessing.center_joints_to_hip"].assert_called()
-    mock_dependencies["run_algorithm"].assert_called()
-    mock_dependencies["writers.save_results_to_ndjson"].assert_called()
 
-
-def test_run_with_valid_file(
-    mock_paths: Tuple[pathlib.Path, pathlib.Path], mock_dependencies: None
-) -> None:
+def test_run_with_valid_file(mock_paths: Tuple[pathlib.Path, pathlib.Path]) -> None:
     """Test run function with a valid file."""
     gold_path, experimental_file = mock_paths
     sequence = 1
     algorithm = "DTW"
 
     orchestrator.run(gold_path, experimental_file, sequence, algorithm)
-
-    mock_dependencies["models.Metadata.get_metadata"].assert_called()
-    mock_dependencies["readers.read_sheet"].assert_called()
-    mock_dependencies["preprocessing.center_joints_to_hip"].assert_called()
-    mock_dependencies["run_algorithm"].assert_called()
-    mock_dependencies["writers.save_results_to_ndjson"].assert_called()
 
 
 def test_run_with_invalid_path() -> None:
