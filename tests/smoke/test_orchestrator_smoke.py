f17d20895d50ee1f6a54b6c95bfd18a9896a0a3e
cgmaiorano <celiagrace13@gmail.com>
2025-03-25 10:02:07 -0400

    resolving edits

--- tests/smoke/test_orchestrator_smoke.py
+++ tests/smoke/test_orchestrator_smoke.py
@@ -1,6 +1,7 @@
 """smoke tests for orchestrator.py."""
 
 import datetime
+import json
 import pathlib
 
 from mobi_motion_tracking.core import orchestrator
@@ -16,7 +17,16 @@ def test_orchestrator_good() -> None:
     expected_output_file = pathlib.Path(
         f"tests/sample_data/results_Gold_{date_str}.ndjson"
     )
+    expected_keys = {"participant_ID", "sheetname", "method", "distance"}
 
     orchestrator.run(experimental_path, gold_path, sequence, algorithm)
 
-    assert expected_output_file.exists(), "Expected file was not created"
+    assert expected_output_file.exists(), "Expected file was not created."
+
+    with open(expected_output_file, "r") as file:
+        last_line = list(file)[-1]
+    data = json.loads(last_line)
+
+    assert expected_keys == set(data.keys()), (
+        "Saved dictionary keys do not match expected results."
+    )
