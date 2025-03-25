7ed5d48ac2063fbc86296e09047d6eb1809716ae
cgmaiorano <celiagrace13@gmail.com>
2025-03-25 15:39:27 -0400

    ruff

--- src/mobi_motion_tracking/processing/similarity_functions.py
+++ src/mobi_motion_tracking/processing/similarity_functions.py
@@ -76,11 +76,13 @@ def dynamic_time_warping(
         elif j == 0:
             i -= 1
         else:
-            min_cost_index = np.argmin([
-                cost_matrix[i - 1, j],
-                cost_matrix[i, j - 1],
-                cost_matrix[i - 1, j - 1],
-            ])
+            min_cost_index = np.argmin(
+                [
+                    cost_matrix[i - 1, j],
+                    cost_matrix[i, j - 1],
+                    cost_matrix[i - 1, j - 1],
+                ]
+            )
             if min_cost_index == 0:
                 i -= 1
             elif min_cost_index == 1:
