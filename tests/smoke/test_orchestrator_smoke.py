59e533edb83709706c86e86d9ad7cf56882fbe7c
cgmaiorano <celiagrace13@gmail.com>
2025-03-25 09:40:57 -0400

    redo smoke test

--- tests/smoke/test_orchestrator_smoke.py
+++ tests/smoke/test_orchestrator_smoke.py
@@ -1,8 +1,7 @@
 """smoke tests for orchestrator.py."""
 
+import datetime
 import pathlib
-
-import pytest
 
 from mobi_motion_tracking.core import orchestrator
 
@@ -13,5 +12,11 @@ def test_orchestrator_good() -> None:
     gold_path = pathlib.Path("tests/sample_data/Gold.xlsx")
     sequence = [1]
     algorithm = "dtw"
+    date_str = datetime.datetime.now().strftime("%m%d%Y")
+    expected_output_file = pathlib.Path(
+        f"tests/sample_data/results_Gold_{date_str}.ndjson"
+    )
 
     orchestrator.run(experimental_path, gold_path, sequence, algorithm)
+
+    assert expected_output_file.exists(), "Expected file was not created"
