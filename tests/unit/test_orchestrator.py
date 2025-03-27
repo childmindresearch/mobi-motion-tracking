53dd704d80e645137e2af0b50e5455df5750aecf
cgmaiorano <celiagrace13@gmail.com>
2025-03-27 14:55:27 -0400

    edits to unit test

--- tests/unit/test_orchestrator.py
+++ tests/unit/test_orchestrator.py
@@ -9,7 +9,7 @@ from mobi_motion_tracking.core import orchestrator
 
 
 def test_run_bad_input_path() -> None:
-    """Tests the run function with an input path that is not a file nor a directory."""
+    """Tests the run function with an unsupported algorithm."""
     file_path = pathlib.Path("/tmp/fake_fifo")
     gold_path = pathlib.Path("tests/sample_data/Gold.xlsx")
     sequence = [1, 2, 3]
