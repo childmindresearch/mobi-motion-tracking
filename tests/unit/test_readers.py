230d0e3060f5e790b68b2af9f41b339f09086793
cgmaiorano <celiagrace13@gmail.com>
2025-03-28 16:36:27 -0400

    ruff format

--- tests/unit/test_readers.py
+++ tests/unit/test_readers.py
@@ -22,17 +22,21 @@ def create_dummy_dataframe(valid: bool = True) -> pd.DataFrame:
         placement or a missing 'x_Hip' for testing.
     """
     if valid:
-        data = pd.DataFrame([
-            list(range(1, 71)),
-            ["a", "b", "c", "d", "e", "x_Hip", "g"] + list(range(8, 71)),
-            list(range(71, 141)),
-        ])
+        data = pd.DataFrame(
+            [
+                list(range(1, 71)),
+                ["a", "b", "c", "d", "e", "x_Hip", "g"] + list(range(8, 71)),
+                list(range(71, 141)),
+            ]
+        )
     else:
-        data = pd.DataFrame([
-            list(range(1, 71)),
-            ["a", "b", "c", "d", "e", "f", "g"] + list(range(8, 71)),
-            list(range(71, 141)),
-        ])
+        data = pd.DataFrame(
+            [
+                list(range(1, 71)),
+                ["a", "b", "c", "d", "e", "f", "g"] + list(range(8, 71)),
+                list(range(71, 141)),
+            ]
+        )
     return data
 
 
@@ -56,14 +60,10 @@ def test_data_cleaner_good() -> None:
     assert cleaned_data.shape == (
         expected_rows,
         expected_cols,
-    ), (
-        f"Expected shape ({expected_rows}, {expected_cols}), \
+    ), f"Expected shape ({expected_rows}, {expected_cols}), \
             but got {cleaned_data.shape}"
-    )
-    assert np.array_equal(cleaned_data, expected_output), (
-        "Extracted data does not \
+    assert np.array_equal(cleaned_data, expected_output), "Extracted data does not \
         match expected values."
-    )
 
 
 def test_data_cleaner_index_error() -> None:
@@ -80,10 +80,8 @@ def test_read_sheet_invalid_sheet_name_continues(
     """Test ValueError when sheet name does not exist."""
     output = readers.read_sheet(sample_excel_path, "InvalidSheet")
 
-    assert len(output) == 0, (
-        "Expected output for an invalid sheet name should be \
+    assert len(output) == 0, "Expected output for an invalid sheet name should be \
         empty."
-    )
 
 
 def test_read_sheet_good(sample_excel_path: pathlib.Path) -> None:
@@ -98,14 +96,10 @@ def test_read_sheet_good(sample_excel_path: pathlib.Path) -> None:
     assert cleaned_data.shape == (
         expected_rows,
         expected_cols,
-    ), (
-        f"Expected shape ({expected_rows}, {expected_cols}), \
+    ), f"Expected shape ({expected_rows}, {expected_cols}), \
             but got {cleaned_data.shape}"
-    )
-    assert np.array_equal(cleaned_data, expected_output), (
-        "Extracted data does not \
+    assert np.array_equal(cleaned_data, expected_output), "Extracted data does not \
         match expected values."
-    )
 
 
 def test_get_metadata_good() -> None:
@@ -120,9 +114,9 @@ def test_get_metadata_good() -> None:
     assert isinstance(participant_ID, str), "participant_ID should be a string."
     assert isinstance(sheetname, str), "sequence should be a string."
     assert expected_ID == participant_ID, "extracted ID does not match expected value."
-    assert expected_seq == sheetname, (
-        "extracted sequence does not match expected value."
-    )
+    assert (
+        expected_seq == sheetname
+    ), "extracted sequence does not match expected value."
 
 
 def test_get_metadata_file_not_found() -> None:
@@ -131,14 +125,12 @@ def test_get_metadata_file_not_found() -> None:
         pathlib.Path("/dummy/path/100.xlsx"), 1
     )
 
-    assert participant_ID == "None", (
-        "Expected ID should be None when a file does not \
+    assert participant_ID == "None", "Expected ID should be None when a file does not \
         exist."
-    )
-    assert sheetname == "None", (
-        "Expected sheetname should be None when a file does not \
+    assert (
+        sheetname == "None"
+    ), "Expected sheetname should be None when a file does not \
         exist."
-    )
 
 
 def test_get_metadata_invalid_file_extension() -> None:
@@ -147,14 +139,12 @@ def test_get_metadata_invalid_file_extension() -> None:
         pathlib.Path("/dummy/path/100.csv"), 1
     )
 
-    assert participant_ID == "None", (
-        "Expected ID should be None when a file does not \
+    assert participant_ID == "None", "Expected ID should be None when a file does not \
         exist."
-    )
-    assert sheetname == "None", (
-        "Expected sheetname should be None when a file does not \
+    assert (
+        sheetname == "None"
+    ), "Expected sheetname should be None when a file does not \
         exist."
-    )
 
 
 def test_get_metadata_incorrect_filename() -> None:
@@ -163,14 +153,12 @@ def test_get_metadata_incorrect_filename() -> None:
         pathlib.Path("/dummy/path/100_01.xlsx"), 1
     )
 
-    assert participant_ID == "None", (
-        "Expected ID should be None when a file does not \
+    assert participant_ID == "None", "Expected ID should be None when a file does not \
         exist."
-    )
-    assert sheetname == "None", (
-        "Expected sheetname should be None when a file does not \
+    assert (
+        sheetname == "None"
+    ), "Expected sheetname should be None when a file does not \
         exist."
-    )
 
 
 def test_read_participant_data_good() -> None:
@@ -179,15 +167,9 @@ def test_read_participant_data_good() -> None:
         pathlib.Path("tests/sample_data/100.xlsx"), 1
     )
 
-    assert participant_data.participant_ID == "100", (
-        f"Expected participant_ID is 100 \
+    assert participant_data.participant_ID == "100", f"Expected participant_ID is 100 \
         not {participant_data.participant_ID}"
-    )
-    assert participant_data.sequence_sheetname == "seq1", (
-        f"Expected sheetname is seq1 \
+    assert participant_data.sequence_sheetname == "seq1", f"Expected sheetname is seq1 \
         not {participant_data.sequence_sheetname}"
-    )
-    assert participant_data.data.shape == (17, 61), (
-        f"Expected shape of sample data is \
+    assert participant_data.data.shape == (17, 61), f"Expected shape of sample data is \
           [18,61] not {participant_data.data.shape}"
-    )
