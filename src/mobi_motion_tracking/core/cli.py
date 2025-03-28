fe86ea0469684cd1e70e6d6cb0ca312ad8af7a46
cgmaiorano <celiagrace13@gmail.com>
2025-03-28 09:34:41 -0400

    added arguments

--- src/mobi_motion_tracking/core/cli.py
+++ src/mobi_motion_tracking/core/cli.py
@@ -17,3 +17,31 @@ def parse_arguments(args: Optional[List[str]] = None) -> argparse.Namespace:
     Returns:
         Namespace object with all the input arguments and default values.
     """
+    parser = argparse.ArgumentParser(
+        description="Run the main motion tracking pipeline.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog="Please report issues at https://github.com/childmindresearch/mobi-motion-tracking.",
+    )
+    parser.add_argument(
+        "--d", "--data", type=pathlib.Path, help="Path to the subject(s) data."
+    )
+
+    parser.add_argument(
+        "-g", "--gold", type=pathlib.Path, help="Path to the gold data file."
+    )
+
+    parser.add_argument(
+        "--s",
+        "--sequence",
+        type=List[int],
+        help="List of integer(s) indicating which sequences to run the pipeline for.",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--algorithm",
+        type=str,
+        choices=["dtw"],
+        default="dtw",
+        help="Pick which algorithm to use. Can be 'dtw'.",
+    )
