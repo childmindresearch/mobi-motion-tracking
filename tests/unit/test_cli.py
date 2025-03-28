d4b881dc97bb6b8b0d7ac21eabf7bb3b3e995be7
cgmaiorano <celiagrace13@gmail.com>
2025-03-28 13:54:32 -0400

    ruff format

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -10,14 +10,16 @@ from mobi_motion_tracking.core import cli, orchestrator
 
 def test_parse_arguments() -> None:
     """Test the basic running of argparse with manadatory args."""
-    args = cli.parse_arguments([
-        "path/to/subject",
-        "path/to/gold",
-        "1",
-        "2",
-        "3",
-        "dtw",
-    ])
+    args = cli.parse_arguments(
+        [
+            "path/to/subject",
+            "path/to/gold",
+            "1",
+            "2",
+            "3",
+            "dtw",
+        ]
+    )
 
     assert args.data == pathlib.Path("path/to/subject")
     assert args.gold == pathlib.Path("path/to/gold")
@@ -37,12 +39,14 @@ def test_main_default(
     """Test cli with only necessary arguments."""
     mock_run = mocker.patch.object(orchestrator, "run")
 
-    cli.main([
-        "tests/sample_data/100.xlsx",
-        "tests/sample_data/Gold.xlsx",
-        "1",
-        "dtw",
-    ])
+    cli.main(
+        [
+            "tests/sample_data/100.xlsx",
+            "tests/sample_data/Gold.xlsx",
+            "1",
+            "dtw",
+        ]
+    )
 
     mock_run.assert_called_once_with(
         experimental_path=pathlib.Path("tests/sample_data/100.xlsx"),
