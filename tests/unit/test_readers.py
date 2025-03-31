569102af6ef10e23abc7ebac51513ff281f7470c
cgmaiorano <celiagrace13@gmail.com>
2025-03-31 16:06:42 -0400

    ruff format

--- tests/unit/test_readers.py
+++ tests/unit/test_readers.py
@@ -22,17 +22,21 @@ def create_dummy_dataframe(valid: bool = True) -> pd.DataFrame:
         placement or a missing 'x_Hip' for testing.
     """
     if valid:
-        data = pd.DataFrame([
-            list(range(1, 71)),
-            ["a", "b", "c", "d", "e", "x_Hip", "g"] + list(range(8, 71)),
-            list(range(71, 141)),
-        ])
+        data = pd.DataFrame(
+            [
+                list(range(1, 71)),
+                ["a", "b", "c", "d", "e", "x_Hip", "g"] + list(range(8, 71)),
+                list(range(71, 141)),
+            ]
+        )
     else:
-        data = pd.DataFrame([
-            list(range(1, 71)),
-            ["a", "b", "c", "d", "e", "f", "g"] + list(range(8, 71)),
-            list(range(71, 141)),
-        ])
+        data = pd.DataFrame(
+            [
+                list(range(1, 71)),
+                ["a", "b", "c", "d", "e", "f", "g"] + list(range(8, 71)),
+                list(range(71, 141)),
+            ]
+        )
     return data
 
 
@@ -56,14 +60,10 @@ def test_data_cleaner_good() -> None:
     assert cleaned_data.shape == (
         expected_rows,
         expected_cols,
-    ), (
-        f"Expected shape ({expected_rows}, {expected_cols}), \
+    ), f"Expected shape ({expected_rows}, {expected_cols}), \
             but got {cleaned_data.shape}"
-    )
-    assert np.array_equal(cleaned_data, expected_output), (
-        "Extracted data does not \
+    assert np.array_equal(cleaned_data, expected_output), "Extracted data does not \
         match expected values."
-    )
 
 
 def test_data_cleaner_index_error() -> None:
@@ -80,18 +80,12 @@ def test_read_participant_data_good() -> None:
         pathlib.Path("tests/sample_data/100.xlsx"), 1
     )
 
-    assert participant_data.participant_ID == "100", (
-        f"Expected participant_ID is 100 \
+    assert participant_data.participant_ID == "100", f"Expected participant_ID is 100 \
         not {participant_data.participant_ID}"
-    )
-    assert participant_data.sequence_sheetname == "seq1", (
-        f"Expected sheetname is seq1 \
+    assert participant_data.sequence_sheetname == "seq1", f"Expected sheetname is seq1 \
         not {participant_data.sequence_sheetname}"
-    )
-    assert participant_data.data.shape == (17, 61), (
-        f"Expected shape of sample data is \
+    assert participant_data.data.shape == (17, 61), f"Expected shape of sample data is \
           [18,61] not {participant_data.data.shape}"
-    )
 
 
 def test_read_participant_data_no_sheet() -> None:
@@ -100,7 +94,7 @@ def test_read_participant_data_no_sheet() -> None:
         pathlib.Path("tests/sample_data/100.xlsx"), 4
     )
 
-    assert participant_data.data.size == 0, (
-        "Expected result to be empty when sheet does not \
+    assert (
+        participant_data.data.size == 0
+    ), "Expected result to be empty when sheet does not \
         exist."
-    )
